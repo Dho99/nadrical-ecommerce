c5f6a37365cc7f410e98277eed7d1903f45c9491
Dho99 <ridhoawwaludin@gmail.com>
2026-09-06 23:16:36 +0700

    refactor(routes): drop customer back-office and wire order detail routes

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { AppLayout } from '../layout/AppLayout'
-import { AdminLayout } from '../layout/AdminLayout'
 import { ProfileLayout } from '../layout/ProfileLayout'
 import { HomePage } from './HomePage'
 import { ProductsPage } from './ProductsPage'
@@ -8,23 +7,17 @@ import { ProductDetailPage } from './ProductDetailPage'
 import { ProductRatingsPage } from './ProductRatingsPage'
 import { CartPage } from './CartPage'
 import { CheckoutPage } from './CheckoutPage'
-import { WishlistPage } from './WishlistPage'
 import { LoginPage } from './LoginPage'
 import { RegisterPage } from './RegisterPage'
 import { NotFoundPage } from './NotFoundPage'
 import { RequireAuth } from './RequireAuth'
 import { ProfilePage } from './ProfilePage'
 import { OrdersHistoryPage } from './OrdersHistoryPage'
+import { OrderDetailPage } from './OrderDetailPage'
+import { RefundPage } from './RefundPage'
 import { AddressBookPage } from './AddressBookPage'
 import { EditProfilePage } from './EditProfilePage'
-import { AdminDashboardPage } from './AdminDashboardPage'
-import { AdminProductsPage } from './AdminProductsPage'
-import { AdminProductFormPage } from './AdminProductFormPage'
-import { AdminOrdersPage } from './AdminOrdersPage'
-import { AdminChatPage } from './AdminChatPage'
-import { AdminDiscountsPage } from './AdminDiscountsPage'
-import { AdminDiscountFormPage } from './AdminDiscountFormPage'
-import { AdminPreordersPage } from './AdminPreordersPage'
+import { ProfileWishlistPage } from './ProfileWishlistPage'
 
 export const router = createBrowserRouter([
   {
@@ -35,7 +28,7 @@ export const router = createBrowserRouter([
       { path: '/products', element: <ProductsPage /> },
       { path: '/products/:id', element: <ProductDetailPage /> },
       { path: '/products/:id/ratings', element: <ProductRatingsPage /> },
-      { path: '/wishlist', element: <WishlistPage /> },
+      { path: '/wishlist', element: <Navigate to="/profile/wishlist" replace /> },
       {
         path: '/cart',
         element: (
@@ -63,30 +56,15 @@ export const router = createBrowserRouter([
           { index: true, element: <ProfilePage /> },
           { path: 'edit', element: <EditProfilePage /> },
           { path: 'orders', element: <OrdersHistoryPage /> },
+          { path: 'orders/:id', element: <OrderDetailPage /> },
+          { path: 'orders/:id/refund', element: <RefundPage /> },
+          { path: 'wishlist', element: <ProfileWishlistPage /> },
           { path: 'addresses', element: <AddressBookPage /> },
         ],
       },
       { path: '/login', element: <LoginPage /> },
       { path: '/register', element: <RegisterPage /> },
       { path: '*', element: <NotFoundPage /> },
-    ],
-  },
-  {
-    path: '/admin',
-    element: <AdminLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      { index: true, element: <Navigate to="/admin/dashboard" replace /> },
-      { path: 'dashboard', element: <AdminDashboardPage /> },
-      { path: 'products', element: <AdminProductsPage /> },
-      { path: 'products/new', element: <AdminProductFormPage /> },
-      { path: 'products/:id/edit', element: <AdminProductFormPage /> },
-      { path: 'discounts', element: <AdminDiscountsPage /> },
-      { path: 'discounts/new', element: <AdminDiscountFormPage /> },
-      { path: 'discounts/:code/edit', element: <AdminDiscountFormPage /> },
-      { path: 'preorders', element: <AdminPreordersPage /> },
-      { path: 'orders', element: <AdminOrdersPage /> },
-      { path: 'chat', element: <AdminChatPage /> },
     ],
   },
 ])
